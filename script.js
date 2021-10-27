eed75040a691b5188f894d73da6d2a0c3704c411
Max Lindahl <Max.Lindahl97@gmail.com>
2021-10-27 23:52:15 +0200

    Setting up base for web shop

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
     document.querySelector('.container').classList.toggle('change');
 });
+
+document.querySelector('.ChangePage').addEventListener('click', () => {
+    document.location.replace("homebuy.html");
+});
